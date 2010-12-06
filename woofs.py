5ec110849fa50060826274bf4dd9e25f228f22b0
Kyle Isom <sadm@kyleisom.net>
2010-12-06 16:00:34 -0700

    Add code to grab IP address, update initialisation code.

--- woofs.py
+++ woofs.py
@@ -6,12 +6,13 @@ python 2 version.
 """
 
 import os
+import pdb
 import ssl
 import socket
 import sys
 import tarfile
 import tempfile
-import traceback
+import urllib2
 
 import httplib as client
 import BaseHTTPServer as server
@@ -25,9 +26,15 @@ class woofs():
     cert            = None
     serv            = None
     sock_type       = None
+    addr            = None
+    external        = None
+    port            = None
+    downloads       = None
     
     
-    def __init__(self, filename = None, ipv6 = False, certificate = None, keyfile = None, confdir = None):
+    def __init__(self, filename = None, ipv6 = False, ip = None, ext = False,
+                 port = None, downloads = 1, certificate = None, keyfile = None,
+                 confdir = None):
 
         # three valid cases:
         #   1. no args are passed to the class: use the configuration dir to
@@ -82,6 +89,22 @@ class woofs():
             self.sock_type  = socket.AF_INET6
         else:
             self.sock_type  = socket.AF_INET
+    
+        # get the IP address
+        if ip:
+            self.addr       = ip
+        elif ext:
+            self.addr       = self._get_external_addr()
+        else:
+            self.addr       = self._get_local_addr()
+            
+        # setting ports
+        if port:
+            self.port       = port
+        else:
+            self.port       = 8000 + int(random.random() * 1000)
+        
+        self.downloads      = downloads
         
         # load the file
         self.load_file(filename)
@@ -173,12 +196,63 @@ class woofs():
         self.file   = f.read( )
         f.close()
     
+    def _get_local_addr(self):
+
+        if self.sock_type == socket.AF_INET:
+            # by sending a UDP datagram to a test net, it is possible to 
+            # determine the default route and the IP address.
+            ip_list     = [ ]                       # list of IP addresses 
+            test_nets   = [ '', '', ''  ]           # RFC 5735 test nets
+            
+            for ip in test_nets:
+               sock     = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+               sock.connect((ip, 0))
+               addr     = sock.getsockname()[0]
+               sock.close()
+               if addr in ip_list: return addr
+               candidates.append(ip_addr)
+        
+        elif self.sock_type == socket.AF_INET6:
+            # flowinfo is zero as per RFC 2553
+            test_net6   = ('2001:DB8::0', 4141, 0, 32)  # RFC 3949 IPv6
+                                                        # documentation prefix
+            sock        = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+            sock.connect(test_net6)
+            addr        = sock.getsockname()[0]
+            sock.close()
+            return addr
+    
+        # bad juju had to happen to get here
+        else:
+            err('the server has an invalid socket type!\n')
+            sys.exit(2)
+
+    def _get_external_addr(self):
+        return urllib2.urlopen('www.whatismyip.org').read()
+    
     def _setup_socket(self):
-        self.serv    = socket.socket( )
+        if socket.AF_INET == self.sock_type:
+            serv_addr   = (self.addr, self.port)
+        elif socket.AF_INET6 == self.sock_type:
+            serv_addr   = (self.addr, self.port, 0, 0)
+        
+        self.serv    = socket.socket(self.sock_type, socket.SOCK_STREAM)
+        self.serv.bind(serv_addr)
+        self.serv.listen(1)
     
     def _setup_SSL(self):
-        self._setup_socket()
+        pass
+        
     
     def _start_listen(self):
-        pass
+        self._setup_socket()
+        
+        for i in range(downloads):
+            (client_socket, client_addr)    = self.serv.accept()
+            cstream     = ssl.wrap_socket(client_socket,
+                                          keyfile       = self.key,
+                                          certfile      = self.cert,
+                                          ssl_version   = ssl.PROTOCOL_TLSv1,
+                                          server_side   = True)
+
     
