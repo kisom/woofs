7e1d4c94ff12dd5703bd734d228c83da418b78b7
Kyle Isom <coder@kyleisom.net>
2011-05-15 22:30:13 -0600

    adding test http socket server

---
+++ http_server.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# file: http_server.py
+# author: kyle isom <coder@kyleisom.net>
+#
+# simple fileserver that displays a line of text and a link to a file
+
+import os
+import socket
+import sys
